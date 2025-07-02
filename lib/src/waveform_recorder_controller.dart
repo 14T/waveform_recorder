6f3e58f93e133c0838836d97bb5a95b5904947b7
Chetan Anand <anand.myemail@gmail.com>
2025-07-03 00:26:37 +0800

    Update waveform_recorder_controller.dart

--- lib/src/waveform_recorder_controller.dart
+++ lib/src/waveform_recorder_controller.dart
@@ -70,7 +70,7 @@ class WaveformRecorderController extends ChangeNotifier {
 
   /// Indicates whether audio recording is currently in progress.
   // bool get isRecording => _audioRecorder != null;
-  bool get isRecording => _audioRecorder.isRecording();
+  bool get isRecording => _audioRecorder?.isRecording() ?? false;
 
   /// Provides a stream of amplitude data for generating the waveform.
   ///
